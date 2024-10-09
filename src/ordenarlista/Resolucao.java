3d431caaec9cfbcf40d9682dafa90df00a7c076d
Kleber Vales <ksvales@outlook.pt>
2024-10-09 07:59:58 -0300

    Classe estudante

---
+++ src/ordenarlista/Resolucao.java
@@ -0,0 +1,30 @@
+package ordenarlista;
+
+class Student {
+	private int id;
+	private String fname;
+	private double cgpa;
+
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public double getCgpa() {
+		return cgpa;
+	}
+}
+
+public class Resolucao {
+
+}
