9aa5e70495c31b4c428e5a0785de11edbada6bc8
Kleber Vales <ksvales@outlook.pt>
2024-10-09 08:01:04 -0300

    Classe comparar

--- src/ordenarlista/Resolucao.java
+++ src/ordenarlista/Resolucao.java
@@ -25,6 +25,21 @@ class Student {
 	}
 }
 
+class StudentComparator implements Comparator<Student> {
+	public int compare(Student s1, Student s2) {
+		if ((s2.getCgpa() != s1.getCgpa())) {
+			return Double.compare(s2.getCgpa(), s1.getCgpa());
+		} else {
+			if (!(s1.getFname().equals(s2.getFname()))) {
+				return s1.getFname().compareTo(s2.getFname());
+			} else {
+				return Integer.compare(s1.getId(), s2.getId());
+			}
+
+		}
+	}
+}
+
 public class Resolucao {
 
 }
